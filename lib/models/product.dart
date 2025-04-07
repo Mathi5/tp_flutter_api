b144575220d476634b3b5adf1cae87f2f4a7a2ca
Mathi5 <75082771+Mathi5@users.noreply.github.com>
2025-04-07 16:24:15 +0200

    Add Product model with JSON serialization

---
+++ lib/models/product.dart
@@ -0,0 +1,24 @@
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final String image;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.image,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'] is int
+          ? (json['price'] as int).toDouble()
+          : json['price'],
+      image: json['image'],
+    );
+  }
+}
